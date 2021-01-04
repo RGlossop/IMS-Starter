f9b4bee0953b979a2ed95c4d4b138d209605585c
RGlossop <RGlossop@qa.com>
2021-01-04 05:31:44 +0000

    IMS-38 created order class

---
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -0,0 +1,68 @@
+package com.qa.ims.persistence.domain;
+
+import java.util.ArrayList;
+
+public class Order {
+	private Long id;
+	private Long cust_ID;
+	private ArrayList<Item> items;
+
+	public Order(Long cust_ID, ArrayList<Item> items) {
+		this.cust_ID = cust_ID;
+		this.items = items;
+	}
+	public Order(Long id, Long cust_ID, ArrayList<Item> items) {
+		this.id = id;
+		this.cust_ID = cust_ID;
+		this.items = items;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Long getCust_ID() {
+		return cust_ID;
+	}
+	public void setCust_ID(Long cust_ID) {
+		this.cust_ID = cust_ID;
+	}
+	public ArrayList<Item> getItems() {
+		return items;
+	}
+	public void setItems(ArrayList<Item> items) {
+		this.items = items;
+	}
+	@Override
+	public String toString() {
+		return "Order [id=" + id + ", cust_ID=" + cust_ID + ", items=" + items + "]";
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (cust_ID == null) {
+			if (other.cust_ID != null)
+				return false;
+		} else if (!cust_ID.equals(other.cust_ID))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (items == null) {
+			if (other.items != null)
+				return false;
+		} else if (!items.equals(other.items))
+			return false;
+		return true;
+	}
+	
+}
